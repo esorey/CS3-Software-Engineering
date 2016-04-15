cd3af3878b7c5ea93c4c190c0bfa4c115a3374df
dinglis <dinglis@caltech.edu>
2016-04-15 13:14:46 -0700

    modified format of config file

--- server.cpp
+++ server.cpp
@@ -26,20 +26,28 @@ int parse_port(const char *config_string) {
     NginxConfig config;
     std::stringstream config_stream(config_string);
     parser.Parse(config_string, &config);
-    return std::stoi(config.statements_.at(0)->tokens_.at(0));
+
+    if (config.statements_.at(0)->tokens_.at(0).compare("port") != 0) {
+        std::cerr << "first line of config file must be port number" << std::endl;
+        return -1;
+    }
+    return std::stoi(config.statements_.at(0)->tokens_.at(1));
 }
 
 int main(int argc, char* argv[])
 {
     if (argc != 2) {
         std::cerr << "usage: ./server <server_config_file>"  << std::endl;
-        return(1);
+        return 1;
     }
     try
     {
         boost::asio::io_service io_service;
         std::string config_file = std::string(argv[1]);
         int port = parse_port(argv[1]);
+        if (port == -1) {
+            return 1;
+        }
         tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), port));
 
         // loop on accepting input and serving the page in response
