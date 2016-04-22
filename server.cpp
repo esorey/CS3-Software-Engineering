1bd0928d18571c23f0e9f7ad377b64b03122191c
Eli Sorey <esorey@caltech.edu>
2016-04-21 21:01:04 -0700

    Add optional echo server functionality

--- server.cpp
+++ server.cpp
@@ -10,6 +10,8 @@
 
 using boost::asio::ip::tcp;
 
+const int max_length = 1024;
+
 // Serves a simple hello world HTML page
 std::string serve_page() {
     std::stringstream ss;
@@ -21,6 +23,7 @@ std::string serve_page() {
     return ss.str();
 }
 
+// Parses a port number from a config file
 int parse_port(const char *config_string) {
     NginxConfigParser parser;
     NginxConfig config;
@@ -34,11 +37,58 @@ int parse_port(const char *config_string) {
     return std::stoi(config.statements_.at(0)->tokens_.at(1));
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "usage: ./server <server_config_file>"  << std::endl;
-        return 1;
+// Handles incoming server requests according to provided parameters
+// If echo is true, the server will echo the HTTP request it receives.
+// Otherwise, it will serve "Hello, world!".
+void handle_request(tcp::socket *sock, bool echo) {
+    try {
+        if (echo) {  // Echo the HTTP request
+            char data[max_length];
+            boost::system::error_code error;
+            size_t length;
+            for (;;) {
+                char data[max_length];
+                boost::system::error_code error;
+                size_t length = sock->read_some(boost::asio::buffer(data), error);
+
+                if (error == boost::asio::error::eof)
+                  break; // Connection closed cleanly by peer.
+                else if (error)
+                  throw boost::system::system_error(error); // Some other error.
+
+                // Echo the request back to the client
+                boost::asio::write(*sock, boost::asio::buffer(data, length));
+                break;
+            }
+        }
+
+        else {  // Serve "Hello, world!"
+            boost::asio::write(*sock, boost::asio::buffer(serve_page()));
+        }
     }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "usage: ./server [-e] <server_config_file> "  << std::endl;
+        exit(1);
+    }
+
+    // Parse command-line args
+    int i;
+    int echo = 0;
+
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-e") == 0)  // Check for echo parameter
+        {
+            echo = 1;  
+        }
+    }
+
+    // Run the server
     try {
         boost::asio::io_service io_service;
         std::string config_file = std::string(argv[1]);
@@ -50,9 +100,10 @@ int main(int argc, char* argv[]) {
 
         // loop on accepting input and serve the page in response
         while(true) {
-            tcp::socket socket(io_service);
-            acceptor.accept(socket);
-            boost::asio::write(socket, boost::asio::buffer(serve_page()));
+            tcp::socket sock(io_service);
+            acceptor.accept(sock);
+            handle_request(&sock, echo);
+
         }
     }
     catch (std::exception& e) {
